a03b6d593a8f45b8365aefa653cf34b856ae2221
Flávio Omar Losada <flavio_gs96@hotmail.com>
2019-12-04 16:13:51 -0300

    Optimizing day-01 first puzzle

--- day-01/puzzle01.js
+++ day-01/puzzle01.js
@@ -1,20 +1,11 @@
 const fs = require('fs');
 const ENCODING = {encoding: 'utf8'};
 
-const fuelRequired = mass => {
-  const result = Math.floor(mass / 3) - 2;
-  return result;
-}
-
 const readFileAndSum = file => {
   const fileContent = fs.readFileSync(file, ENCODING);
-  const values = fileContent.split('\n');
-  let accumulator = 0;
-  values.forEach(value => {    
-    value = Number(value.trim());
-    accumulator += fuelRequired(value);
-  });
-  return accumulator;
+  let values = fileContent.split('\n');
+  values = values.map(mass => Math.floor(Number(mass.trim()) / 3) - 2);
+  return values.reduce((accumulator, value) => accumulator + value);
 }
 
 console.log(readFileAndSum(__dirname + '/puzzle01.input'));
